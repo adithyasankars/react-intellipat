7f734c8f94d7ea334293dc3bdfeb994e7cf05f04
Adithyasankar74 <devuktm966@gmail.com>
2024-05-02 11:35:25 +0530

    diff-functions-arrow

--- Hands-on-3/script.js
+++ Hands-on-3/script.js
@@ -1,13 +1,22 @@
-if (true){
-    var x = 10;
-    let y = 20;
-    const z = 30;
+// if (true){
+//     var x = 10;
+//     let y = 20;
+//     const z = 30;
 
-    console.log(x);
-    console.log(y);
-    console.log(z);
+//     console.log(x);
+//     console.log(y);
+//     console.log(z);
+// }
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+const greetPersonOld = function(name){
+    return "Hello " + name;
 }
 
-console.log(x);
-console.log(y);
-console.log(z);
+const greetPerson = name => 'Hello ' + name;
+
+console.log(greetPersonOld('John'));
+console.log(greetPerson('Jane'));
