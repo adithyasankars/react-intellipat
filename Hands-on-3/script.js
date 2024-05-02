5398af0dfabed337d5aca5783174d0f99dd4df49
Adithyasankar74 <devuktm966@gmail.com>
2024-05-02 11:29:46 +0530

    JS added

---
+++ Hands-on-3/script.js
@@ -0,0 +1,13 @@
+if (true){
+    var x = 10;
+    let y = 20;
+    const z = 30;
+
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+
+console.log(x);
+console.log(y);
+console.log(z);
